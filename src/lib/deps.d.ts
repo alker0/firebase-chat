9c24048b04e7cf0ac4a2bf08a281dfa4ba5204b1
alker <alker8h@gmail.com>
2020-07-27 00:33:26 +0000

    Move dependencies into src/lib/deps together

---
+++ src/lib/deps.d.ts
@@ -0,0 +1,4 @@
+export { default as Ferp } from 'https://cdn.skypack.dev/ferp'
+// export { default as Inferno } from 'https://cdn.skypack.dev/inferno'
+export { default as Inferno } from 'inferno'
+export { default as clsx } from 'https://cdn.skypack.dev/clsx'
