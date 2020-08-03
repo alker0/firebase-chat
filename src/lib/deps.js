0e8b6f6b0aedfdebb49111153cbbffe61df065fa
alker <alker8h@gmail.com>
2020-08-03 22:55:24 +0000

    Import only "render" and "Component" from Inferno in deps.js

--- src/lib/deps.js
+++ src/lib/deps.js
@@ -5,7 +5,10 @@ import * as _Ferp from 'https://cdn.skypack.dev/ferp@^1.2.0/src/ferp.js'
 export const Ferp = _Ferp
 
 // import * as _Inferno from 'https://cdn.skypack.dev/inferno@^7.4.2'
-import * as _Inferno from 'inferno'
-export const Inferno = _Inferno
+import {render as infernoRender, Component as infernoComponent} from 'inferno'
+export const Inferno = {
+  render: infernoRender,
+  Component: infernoComponent
+}
 
 export { default as clsx } from "https://cdn.skypack.dev/clsx@^1.1.1/dist/clsx.m.js"
