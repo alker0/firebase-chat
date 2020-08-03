aa24b5928fc45ca50c3f5a8ccfdf5c932980cd6b
alker <alker8h@gmail.com>
2020-08-03 23:01:33 +0000

    Fix import in deps

--- src/lib/deps.d.ts
+++ src/lib/deps.d.ts
@@ -1,4 +1,7 @@
 export { default as Ferp } from 'https://cdn.skypack.dev/ferp'
+
 // export { default as Inferno } from 'https://cdn.skypack.dev/inferno'
-export { default as Inferno } from 'inferno'
+import { render, Component } from 'inferno'
+export const Inferno = { render, Component }
+
 export { default as clsx } from 'https://cdn.skypack.dev/clsx'
--- src/lib/deps.js
+++ src/lib/deps.js
@@ -5,10 +5,7 @@ import * as _Ferp from 'https://cdn.skypack.dev/ferp@^1.2.0/src/ferp.js'
 export const Ferp = _Ferp
 
 // import * as _Inferno from 'https://cdn.skypack.dev/inferno@^7.4.2'
-import {render as infernoRender, Component as infernoComponent} from 'inferno'
-export const Inferno = {
-  render: infernoRender,
-  Component: infernoComponent
-}
+import {render, Component} from 'inferno'
+export const Inferno = { render, Component }
 
 export { default as clsx } from "https://cdn.skypack.dev/clsx@^1.1.1/dist/clsx.m.js"
