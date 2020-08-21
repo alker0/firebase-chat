0774e78e44ea7938532cd5eee8b9313f783307a9
alker <alker8h@gmail.com>
2020-08-21 04:39:00 +0000

    Fix babel-config and remove babel-inferno

--- babel.config.js
+++ babel.config.js
@@ -20,16 +20,9 @@ module.exports = {
     ],
   ],
   plugins: [
-    // ['styled-jsx/babel', {styleModule: '/lib/inferno-styled-jsx.js'}],
     ['styled-jsx/babel', { styleModule: '/web_modules/solid-styled-jsx.js' }],
     'macros',
     '@babel/proposal-class-properties',
     '@babel/proposal-object-rest-spread',
-    // ↓ not working
-    // ['transform-rename-import', {
-    //   replacements: [
-    //     {original: 'inferno', replacement: 'https://cdn.skypack.dev/inferno@^7.4.2'}
-    //   ]
-    // }]
   ],
 };
