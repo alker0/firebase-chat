1cc5c45211cdca16ab420dfc6f6b181256ede13e
alker <alker8h@gmail.com>
2020-09-05 01:39:17 +0000

    Fix component utils

---
+++ src/components/common/src/util/cirrus-utils.ts
@@ -0,0 +1,15 @@
+export const sizeSuffixMap = {
+  xsmall: '-xsmall',
+  small: '-small',
+  normal: '',
+  large: '-large',
+  xlarge: '-xlarge',
+} as const
+
+export const buttonSizeMap = {
+  tiny: 'btn-tiny',
+  small: 'btn-small',
+  normal: 'btn',
+  large: 'btn-large',
+  xlarge: 'btn-xlarge',
+} as const
