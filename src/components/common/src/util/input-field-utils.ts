6d6a4847b6b178598f48e2f4ee26537dae7a5d92
alker <alker8h@gmail.com>
2020-09-12 01:22:08 +0000

    Add strong password regex to input-field-utils.ts

--- src/components/common/src/util/input-field-utils.ts
+++ src/components/common/src/util/input-field-utils.ts
@@ -4,11 +4,16 @@ export const inputRegex = {
   // email: RegExp('^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
   email: '^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)*$',
   password: (length: number = 6) => `^[a-zA-Z0-9]{${length},}$`,
+  strongPassword: (length: number = 8) =>
+    `^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{${length},}$`,
   get emailRegex(): RegExp {
     return RegExp(inputRegex.email);
   },
   get passwordRegex(): (lengtn: number) => RegExp {
     return (length = 6) => RegExp(inputRegex.password(length));
+  },
+  get strongPasswordRegex(): (lengtn: number) => RegExp {
+    return (length = 6) => RegExp(inputRegex.strongPassword(length));
   },
 } as const;
 
