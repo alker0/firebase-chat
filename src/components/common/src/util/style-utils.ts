af7a11e0573e307ae9363680364f2cd34d6cff82
alker <alker8h@gmail.com>
2020-09-26 03:40:54 +0000

    Add styleUtilRegistry to style-utils.ts

--- src/components/common/src/util/style-utils.ts
+++ src/components/common/src/util/style-utils.ts
@@ -1,8 +1,18 @@
 import { css } from 'styled-jsx/css';
 import { createRoot } from 'solid-js';
 
+export interface CSSResolveResult {
+  className: string;
+  styles: string;
+}
+
+export interface DisposableStyle {
+  className: string;
+  dispose: () => void;
+}
+
 export const createDisposableStyle = <T extends string>(
-  cssResolve: () => { className: string; styles: string },
+  cssResolve: () => CSSResolveResult,
 ) =>
   createRoot((dispose) => ({
     className: cssResolve().className as T,
@@ -17,22 +27,80 @@ export const createMultiDisposableStyle = <T extends string[]>(
     dispose,
   }));
 
-export type OnlyWrap = 'only wrap style';
+export interface StyleRegistry
+  extends Partial<Record<string, DisposableStyle>> {}
 
-export const onlyWrap = createRoot(
-  () => css.resolve`
-    * {
-      display: contents;
-    }
-  `,
-).className as OnlyWrap;
+function registerStyle<T extends StyleRegistry, U extends string>(
+  registry: T,
+  key: U,
+  cssResolve: () => CSSResolveResult,
+): asserts registry is T & Record<U, DisposableStyle> {
+  if (registry[key]) return;
 
-export type PointerCursor = 'pointer cursor style';
+  const { className, dispose } = createDisposableStyle(cssResolve);
 
-export const pointerCursor = createRoot(
-  () => css.resolve`
-    * {
-      cursor: pointer;
-    }
-  `,
-).className as PointerCursor;
+  Object.defineProperty(registry, key, {
+    value: {
+      className,
+      dispose: () => {
+        dispose();
+        // eslint-disable-next-line no-param-reassign
+        delete registry[key];
+      },
+    },
+    configurable: true,
+    enumerable: true,
+  });
+}
+
+export const styleUtils = {
+  registry: {} as StyleRegistry,
+
+  registerAndGet<T extends string>(
+    key: T,
+    cssResolve: () => CSSResolveResult,
+  ): DisposableStyle {
+    registerStyle(styleUtils.registry, key, cssResolve);
+
+    return styleUtils.registry[key];
+  },
+
+  onlyWrap: () =>
+    styleUtils.registerAndGet(
+      'onlyWrap',
+      () => css.resolve`
+        * {
+          display: contents;
+        }
+      `,
+    ),
+  pointerCursor: () =>
+    styleUtils.registerAndGet(
+      'pointerCursor',
+      () => css.resolve`
+        * {
+          cursor: pointer;
+        }
+      `,
+    ),
+  noSideMargin: () =>
+    styleUtils.registerAndGet(
+      'noSideMargin',
+      () => css.resolve`
+        * {
+          margin-left: 0;
+          margin-right: 0;
+        }
+      `,
+    ),
+  noSidePadding: () =>
+    styleUtils.registerAndGet(
+      'noSidePadding',
+      () => css.resolve`
+        * {
+          padding-left: 0;
+          padding-right: 0;
+        }
+      `,
+    ),
+};
