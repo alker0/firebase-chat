2dfad60a2bd0d4d1f1d2d46a97974ffc972d3fbe
alker <alker8h@gmail.com>
2020-10-04 05:20:12 +0000

    Update dependencies

--- posthtml.config.js
+++ posthtml.config.js
@@ -15,7 +15,7 @@ module.exports = {
       locals: {
         env: nodeEnv,
         cirrusVersion: '@0.5.5',
-        firebaseVersion: '7.21.1',
+        firebaseVersion: '7.22.0',
         firebaseInitPath: '/lib/firebase-init-app.js',
         firebaseSdk: ['auth', 'database', 'storage'],
         useAnalytics: true,
