e9d3fd6d03182681b0b30ec00124397e4ca53c5c
alker <alker8h@gmail.com>
2020-10-07 23:02:02 +0000

    Use inline script in firebase-sdk.html

--- posthtml.config.js
+++ posthtml.config.js
@@ -1,10 +1,20 @@
 const extend = require('posthtml-extend');
 const expressoins = require('posthtml-expressions');
 const include = require('posthtml-include');
+const packagesInfo = require('./package.json');
 
 const nodeEnv = process.env.NODE_ENV || 'development';
 
 console.error('posthtml.config.js', nodeEnv);
+
+const devDeps = packagesInfo.devDependencies;
+
+const onlyVersion = (semVer) => semVer.replace(/[^\d.]/, '');
+
+const firebaseInitPath =
+  nodeEnv === 'production'
+    ? '/__/firebase/init.js'
+    : '/lib/firebase-init-app.js';
 
 module.exports = {
   input: 'src/templates/*.posthtml',
@@ -13,15 +23,24 @@ module.exports = {
     include({ root: 'src/templates' }),
     expressoins({
       locals: {
-        env: nodeEnv,
+        mode: nodeEnv,
         cirrusVersion: '@0.5.5',
-        firebaseVersion: '7.22.1',
-        firebaseInitPath: '/lib/firebase-init-app.js',
+        firebaseVersion: onlyVersion(devDeps.firebase),
+        firebaseInitPath,
         firebaseSdk: ['auth', 'database', 'storage'],
         useAnalytics: true,
-        firebaseAnalyticsPath: '/lib/firebase-init-analytics.js',
-        useFirebaseUI: true,
         firebaseUIVersion: '3.5.2',
+        firebaseConfig: {
+          apiKey: 'AIzaSyCUDlFQJZdo3NOIAHSt8NmgF-gOHQ9ZkHg',
+          authDomain: 'talker-v1.firebaseapp.com',
+          // databaseURL: 'https://talker-v1.firebaseio.com',
+          databaseURL: 'http://localhost:9000',
+          projectId: 'talker-v1',
+          storageBucket: 'talker-v1.appspot.com',
+          messagingSenderId: '578515840439',
+          appId: '1:578515840439:web:2b7905e64ae01d07778c32',
+          measurementId: 'G-S42EYX1LN4',
+        },
       },
     }),
   ],
