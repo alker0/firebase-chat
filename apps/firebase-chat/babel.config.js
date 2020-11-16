6de5d3c9388ab33246deaef52155ad7ac23cb546
alker <alker8h@gmail.com>
2020-11-16 22:45:23 +0000

    Move application items into apps/

---
+++ apps/firebase-chat/babel.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/env',
+      {
+        modules: false,
+        targets: {
+          esmodules: true,
+        },
+      },
+    ],
+    'solid',
+    [
+      '@babel/typescript',
+      {
+        isTSX: true,
+        allowDeclareFields: true,
+        allExtensions: true,
+      },
+    ],
+  ],
+  plugins: [
+    ['styled-jsx/babel', { styleModule: '/web_modules/solid-styled-jsx.js' }],
+    'macros',
+    '@babel/proposal-class-properties',
+    '@babel/proposal-object-rest-spread',
+    ...(process.env.NODE_ENV === 'production'
+      ? ['transform-remove-console']
+      : []),
+  ],
+};
---
+++ apps/firebase-chat/postcss.config.js
@@ -0,0 +1,10 @@
+const postcssConfig = (ctx) => ({
+  plugins: {
+    'postcss-import': {},
+    '@fullhuman/postcss-purgecss': ctx.env === 'production' && {
+      content: ['./src/templates/**/*.html'],
+    },
+  },
+});
+
+module.exports = postcssConfig;
