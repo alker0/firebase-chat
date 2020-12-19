2dbf845c8af9029cf5c5b2d20143105335ce8a8c
alker <alker8h@gmail.com>
2020-12-19 10:55:21 +0900

    Add firebase SDK types file into firebase-chat/src/typings/

---
+++ apps/firebase-chat/src/typings/firebase-sdk.d.ts
@@ -0,0 +1,6 @@
+import firebase from 'firebase';
+
+export declare type FirebaseUser = firebase.User;
+export declare type FirebaseAuth = firebase.auth.Auth;
+export declare type FirebaseDb = firebase.database.Database;
+export declare type FirebaseDbServerValue = typeof firebase.database.ServerValue;
