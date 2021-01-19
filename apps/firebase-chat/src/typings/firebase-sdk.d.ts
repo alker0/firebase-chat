f0dbe625895eb44e2bc44bdded06de55568c2d1f
alker <alker8h@gmail.com>
2021-01-19 09:01:37 +0900

    Add `FirebaseDbSnapshot` type declaration

--- apps/firebase-chat/src/typings/firebase-sdk.d.ts
+++ apps/firebase-chat/src/typings/firebase-sdk.d.ts
@@ -3,4 +3,5 @@ import firebase from 'firebase';
 export declare type FirebaseUser = firebase.User;
 export declare type FirebaseAuth = firebase.auth.Auth;
 export declare type FirebaseDb = firebase.database.Database;
+export declare type FirebaseDbSnapshot = firebase.database.DataSnapshot;
 export declare type FirebaseDbServerValue = typeof firebase.database.ServerValue;
