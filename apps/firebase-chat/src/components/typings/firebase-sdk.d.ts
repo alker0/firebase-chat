397aadd507d4b87112ad9bb54369575295fc25c2
alker0 <alker8h@gmail.com>
2021-01-28 07:28:25 +0900

    Use `export * from ...` syntax in components/typings/firebase-sdk.d.ts

--- apps/firebase-chat/src/components/typings/firebase-sdk.d.ts
+++ apps/firebase-chat/src/components/typings/firebase-sdk.d.ts
@@ -1,6 +1,1 @@
-import firebase from 'firebase';
-
-export declare type FirebaseAuth = firebase.auth.Auth;
-export declare type FirebaseDb = firebase.database.Database;
-export declare type FirebaseDbSnapshot = firebase.database.DataSnapshot;
-export declare type FirebaseDbServerValue = typeof firebase.database.ServerValue;
+export * from '../../typings/firebase-sdk';
