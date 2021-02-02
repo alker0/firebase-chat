60175b87dea18efabacfe37b627bad10d22d8b0b
alker0 <alker8h@gmail.com>
2021-02-02 19:23:10 +0900

    Add typing for solid resource state objects

--- apps/firebase-chat/src/typings/solid-utils.d.ts
+++ apps/firebase-chat/src/typings/solid-utils.d.ts
@@ -1,3 +1,11 @@
 import { State } from 'solid-js';
 
 export type FromState<T> = T extends State<infer R> ? R : never;
+
+export interface LoadingObject<T> {
+  loading: Record<keyof T, boolean>;
+}
+
+export type ResourceState<T> = State<T & LoadingObject<T>>;
+
+export type StateOrResource<T> = State<T & Partial<LoadingObject<T>>>;
