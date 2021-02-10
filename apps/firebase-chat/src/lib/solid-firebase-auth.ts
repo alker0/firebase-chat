9797751d793e3394691f8c35008d02afe8a7d444
alker0 <alker8h@gmail.com>
2021-02-11 08:21:37 +0900

    Don't have currentUserOrAnonymously property in sessionState

--- apps/firebase-chat/src/lib/solid-firebase-auth.ts
+++ apps/firebase-chat/src/lib/solid-firebase-auth.ts
@@ -1,10 +1,4 @@
-import {
-  createState,
-  createRoot,
-  batch,
-  reconcile,
-  assignProps,
-} from 'solid-js';
+import { createState, createRoot, batch, reconcile } from 'solid-js';
 import { FirebaseAuth, FirebaseUser } from '../typings/firebase-sdk';
 
 export type UserState = FirebaseUser | null;
@@ -12,41 +6,21 @@ export type UserState = FirebaseUser | null;
 export interface SessionStateWhenLoggedIn {
   readonly currentUser: FirebaseUser;
   readonly isLoggedIn: true;
-  readonly currentUserOrAnounymous: () => FirebaseUser;
 }
 
 export interface SessionStateWhenLoggedOut {
   readonly currentUser: null;
   readonly isLoggedIn: false;
-  readonly currentUserOrAnounymous: (auth: FirebaseAuth) => Promise<UserState>;
 }
 
 export type SessionState = SessionStateWhenLoggedIn | SessionStateWhenLoggedOut;
-export type SessionStateWithoutAnonymous =
-  | Omit<SessionStateWhenLoggedIn, 'currentUserOrAnounymous'>
-  | Omit<SessionStateWhenLoggedOut, 'currentUserOrAnounymous'>;
 
-export const [sessionState, setSessionState] = createRoot(() => {
-  const [
-    sessionStateWithoutAnonymous,
-    setState,
-  ] = createState<SessionStateWithoutAnonymous>({
+export const [sessionState, setSessionState] = createRoot(() =>
+  createState<SessionState>({
     currentUser: null,
     isLoggedIn: false,
-  });
-  return [
-    assignProps(sessionStateWithoutAnonymous, {
-      get currentUserOrAnounymous(): SessionState['currentUserOrAnounymous'] {
-        if (sessionStateWithoutAnonymous.isLoggedIn) {
-          return () => sessionStateWithoutAnonymous.currentUser;
-        } else {
-          return async (auth) => (await auth.signInAnonymously()).user;
-        }
-      },
-    }) as SessionState,
-    setState,
-  ];
-});
+  }),
+);
 
 export const sessionStateChangedHandler = (user: UserState) => {
   if (user && !user.isAnonymous && !user.emailVerified) {
